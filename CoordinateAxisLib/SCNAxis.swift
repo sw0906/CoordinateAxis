b91bea5a2e1fe925b5a0c503efb6fa0c5cbc3050
Shou Wei <ws@tectusdreamlab.com>
2018-07-19 17:58:54 +0800

    Update SCNAxis

--- CoordinateAxisLib/SCNAxis.swift
+++ CoordinateAxisLib/SCNAxis.swift
@@ -24,6 +24,62 @@ public struct SCNXYZAxisDefaultValue {
     }
 }
 
+open class SCNAxis: SCNNode {
+    
+    public enum SCNAxisType: String {
+        case X,Y,Z
+    }
+    
+    public var axisNode: SCNNode = SCNNode()
+    public var axisNameNode: SCNNode = SCNNode()
+    public var boxNode: SCNNode = SCNNode()
+    
+    public var hideTestBox = true
+    public var hideName = false
+    
+    public var type: SCNAxisType = .X
+    
+    public init(type: SCNAxisType, radius: CGFloat = 0.005, length: CGFloat = 0.06, color: UIColor = .white, name: String = "x") {
+        super.init()
+        self.type = type
+        addAxis(radius: radius, length: length, color: color, name: name)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addAxis(radius: CGFloat, length: CGFloat, color: UIColor, name: String) {
+        axisNode = SCNNode.creatSCNAxisNode(radius: radius, length: length, color: color)
+        axisNameNode = SCNNode.creatSCNAxisNameNode(name: name, color: color)
+        boxNode = SCNNode.creatSCNAxisBox(color: color)
+        self.addChildNode(axisNode)
+        self.addChildNode(axisNameNode)
+        self.addChildNode(boxNode)
+        
+        boxNode.isHidden = hideTestBox
+        axisNameNode.isHidden = hideName
+    }
+    
+    var translateSetup: SCNVector3 = SCNVector3() {
+        didSet {
+            axisNode.transform = SCNMatrix4Translate(axisNode.transform, translateSetup.x / 2, translateSetup.y / 2, translateSetup.z / 2)
+            boxNode.transform = SCNMatrix4Translate(boxNode.transform, translateSetup.x, translateSetup.y, translateSetup.z)
+            axisNameNode.transform = SCNMatrix4Translate(axisNameNode.transform, translateSetup.x, translateSetup.y, translateSetup.z)
+            
+            let offsetRatio: Float = 0.05
+            switch type {
+            case .X:
+                axisNameNode.transform = SCNMatrix4Translate(axisNameNode.transform, translateSetup.x * offsetRatio, 0, 0)
+            case .Y:
+                axisNameNode.transform = SCNMatrix4Translate(axisNameNode.transform, 0, translateSetup.y * offsetRatio, 0)
+            case .Z:
+                axisNameNode.transform = SCNMatrix4Translate(axisNameNode.transform, 0, translateSetup.y * offsetRatio, translateSetup.z * offsetRatio)
+            }
+        }
+    }
+}
+
 open class SCNXYZAxis: SCNNode {
     
     public var xAxis : SCNAxis = SCNAxis(type: .X)
@@ -86,9 +142,9 @@ open class SCNXYZAxis: SCNNode {
         yAxis.removeFromParentNode()
         zAxis.removeFromParentNode()
         
-        xAxis = SCNAxis.init(type:.X, radius: radius, length: sizeVector.x.cgFloat, color: color.x, name: name.x)
-        yAxis = SCNAxis.init(type:.Y, radius: radius, length: sizeVector.y.cgFloat, color: color.y, name: name.y)
-        zAxis = SCNAxis.init(type:.Z, radius: radius, length: sizeVector.z.cgFloat, color: color.z, name: name.z)
+        xAxis = SCNAxis(type: .X, radius: radius, length: CGFloat(sizeVector.x), color: color.x, name: name.x)
+        yAxis = SCNAxis(type: .Y, radius: radius, length: CGFloat(sizeVector.y), color: color.y, name: name.y)
+        zAxis = SCNAxis(type: .Z, radius: radius, length: CGFloat(sizeVector.z), color: color.z, name: name.z)
         self.addChildNode(xAxis)
         self.addChildNode(yAxis)
         self.addChildNode(zAxis)
@@ -103,53 +159,6 @@ open class SCNXYZAxis: SCNNode {
         zAxis.translateSetup = SCNVector3(0, 0, transVector.z)
     }
     
-}
-
-open class SCNAxis: SCNNode {
-    
-    public enum SCNAxisType {
-        case X,Y,Z
-    }
-    
-    public var axisNode: SCNNode = SCNNode()
-    public var axisNameNode: SCNNode = SCNNode()
-    public var boxNode: SCNNode = SCNNode()
-    
-    public var hideTestBox = true
-    public var hideName = false
-    
-    public var type: SCNAxisType = .X
-    
-    public init(type: SCNAxisType, radius: CGFloat = 0.005, length: CGFloat = 0.06, color: UIColor = .white, name: String = "x") {
-        super.init()
-        self.type = type
-        addAxis(radius: radius, length: length, color: color, name: name)
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func addAxis(radius: CGFloat, length: CGFloat, color: UIColor, name: String) {
-        axisNode = SCNNode.creatSCNAxisNode(radius: radius, length: length, color: color)
-        axisNameNode = SCNNode.creatSCNAxisNameNode(name: name, color: color)
-        boxNode = SCNNode.creatSCNAxisBox(color: color)
-        self.addChildNode(axisNode)
-        self.addChildNode(axisNameNode)
-        self.addChildNode(boxNode)
-        
-        boxNode.isHidden = hideTestBox
-        axisNameNode.isHidden = hideName
-    }
-    
-    var translateSetup: SCNVector3 = SCNVector3() {
-        didSet {
-            let offset: Float = 1.02
-            axisNode.transform = SCNMatrix4Translate(axisNode.transform, translateSetup.x / 2, translateSetup.y / 2, translateSetup.z / 2)
-            boxNode.transform = SCNMatrix4Translate(boxNode.transform, translateSetup.x, translateSetup.y, translateSetup.z)
-            axisNameNode.transform = SCNMatrix4Translate(axisNameNode.transform, translateSetup.x * offset , translateSetup.y * (offset + 0.04), translateSetup.z * offset)
-        }
-    }
 }
 
 extension SCNNode {
